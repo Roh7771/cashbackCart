5518beacaf8394f0bc1c887d209910d598d3c5db
Belov Roman <belov.rom-77@yandex.ru>
2019-10-06 09:16:46 +0300

    Добавлен блок для else

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ if (purchaseCategory === 'usual') {
     cashbackPercent = 0.01;
 } else if (purchaseCategory === 'increased') {
     cashbackPercent = 0.05;
-} else cashbackPercent = 0.3;
+} else {
+    cashbackPercent = 0.3;
+}
 
 console.log(cashbackPercent);
 
